1e64d955ca6d5a129b433d0a3edcf6fa054e3da9
Sebastien Mirolo <smirolo@fortylines.com>
2013-08-20 14:38:49 -0700

    update use of Enum based on chisel docs

--- examples/src/main/scala/Parity.scala
+++ examples/src/main/scala/Parity.scala
@@ -8,7 +8,7 @@ class Parity extends Module {
   val io = new Bundle {
     val in  = Bool(INPUT)
     val out = Bool(OUTPUT) }
-  val s_even :: s_odd :: Nil = Enum(2){ UInt() }
+  val s_even :: s_odd :: Nil = Enum(UInt(), 2)
   val state  = Reg(init=s_even)
   when (io.in) {
     when (state === s_even) { state := s_odd  }
