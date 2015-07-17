101467deffb6647e3ecd683f42b44ce5bf781e85
Jim Lawson <ucbjrl@berkeley.edu>
2015-07-17 08:10:40 -0700

    Fix typo in VecShiftRegisterSimple Reg(Vec()); avoid Chisel data in Adder4.scala

--- problems/VecShiftRegisterSimple.scala
+++ problems/VecShiftRegisterSimple.scala
@@ -7,7 +7,7 @@ class VecShiftRegisterSimple extends Module {
     val in  = UInt(INPUT,  8)
     val out = UInt(OUTPUT, 8)
   }
-  val delays = Reg(Vec(UInt(width = 8)), 4)
+  val delays = Reg(Vec(UInt(width = 8), 4))
   /// fill in here ...
   io.out := UInt(0)
 }
