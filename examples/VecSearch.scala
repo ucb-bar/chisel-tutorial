17ee006e15606333a03f9fd5e3a0dc0d7e72ef65
Jim Lawson <ucbjrl@berkeley.edu>
2015-07-22 11:24:01 -0700

    use cloneType() instead of clone(); use independent literal pattern (of the graph).

--- examples/VecSearch.scala
+++ examples/VecSearch.scala
@@ -2,21 +2,22 @@ package TutorialExamples
 
 import Chisel._
 
+object VecSearchTest {
+  val pattern = Array(0, 4, 15, 14, 2, 5, 13)
+}
+
 class VecSearch extends Module {
   val io = new Bundle {
     val out = UInt(OUTPUT,  4)
   }
   val index = Reg(init = UInt(0, width = 3))
-  val elts  = Vec(UInt(0), UInt(4), UInt(15), UInt(14),
-                  UInt(2), UInt(5), UInt(13))
-  // val elts  = Vec.fill(8){ Reg(UInt(width = 4)) }
-  // elts(index) := index
+  val elts  = Vec(VecSearchTest.pattern.map(UInt(_, 4)))
   index := index + UInt(1)
   io.out := elts(index)
 }
 
 class VecSearchTests(c: VecSearch) extends Tester(c) {
-  val list = c.elts.map(int(_)) 
+  val list = VecSearchTest.pattern
   for (elt <- list) {
     expect(c.io.out, elt)
     step(1)
