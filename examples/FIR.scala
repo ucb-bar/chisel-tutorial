e8c80cac987c2b2b4bbb7c44671301dae1999438
Jim Lawson <ucbjrl@berkeley.edu>
2015-08-04 13:24:49 -0700

    Initialize RegNext()'s.

--- examples/FIR.scala
+++ examples/FIR.scala
@@ -4,19 +4,19 @@ import Chisel._
 import scala.collection.mutable.ArrayBuffer
 
 object Delays {
-  def apply[U <: Data](x: U, n: Int): List[U] = 
-    if (n <= 1) List(x) else x :: Delays(RegNext(x), n-1)
+  def apply[U <: Data](x: U, n: Int, i: U): List[U] = 
+    if (n <= 1) List(x) else x :: Delays(RegNext(x, init=i), n-1, i)
 }
 
 object GenFIR {
-  def apply[T <: Data with Num[T]](ws: Seq[T], x: T): T = 
-    (ws, Delays(x, ws.length)).zipped.map( _ * _ ).reduce( _ + _ )
+  def apply[T <: Data with Num[T]](ws: Seq[T], x: T, i: T): T = 
+    (ws, Delays(x, ws.length, i)).zipped.map( _ * _ ).reduce( _ + _ )
 }
 
 class FIR extends Module {
   val io = new Bundle { val x = Flo(INPUT); val z = Flo(OUTPUT) }
   val ws = Array(Flo(0.25), Flo(0.75))
-  io.z  := GenFIR(ws, io.x)
+  io.z  := GenFIR(ws, io.x, Flo(0))
 }
 
 // FIR filter
